db861ac3edf714eaedf8e1695deae297d8331500
hhikmetf <58308917+hhikmetf@users.noreply.github.com>
2023-06-22 17:41:05 +0300

    Update OyunKontrol.cs

--- Assets/Scripts/OyunKontrol.cs
+++ Assets/Scripts/OyunKontrol.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class OyunKontrol : MonoBehaviour {
 	public bool oyunAktif = true;
-	public int altinSayisi = 0;
-	public UnityEngine.UI.Text altinSayisiText;
-	public UnityEngine.UI.Button baslaButonu;
+
+
 	// Use this for initialization
 	void Start () {
 		GetComponent<AudioSource> ().Play ();
@@ -17,13 +16,9 @@ public class OyunKontrol : MonoBehaviour {
 		
 	}
 
-	public void AltinArttir(){
-		altinSayisi += 1;
-		altinSayisiText.text = "" + altinSayisi;
-	}
+
 
 	public void OyunaBasla(){
 		oyunAktif = true;
-		baslaButonu.gameObject.SetActive (false);
 	}
 }
